adefc078267d26e38897b8aeea95017c8ac45c5d
knowaraj <sendmailtoaraj@gmail.com>
2025-12-23 14:17:05 +0545

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    port:3306,
+    username:'root',
+    password:'',
+    database:'nestdb',
+    entities:[],
+    synchronize:true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
